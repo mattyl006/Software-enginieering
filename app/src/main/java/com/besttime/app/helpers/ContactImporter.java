a9d3a351f2c19db587c506bb67339d3b9de62849
Michał Czekański <michal.cz2202@gmai.com>
2020-01-04 17:37:57 +0100

    Add check and request read contact permission
    Added because dangerous permissions like contact reading have to be
    granted by user at app runtime.

--- app/src/main/java/com/besttime/app/helpers/ContactImporter.java
+++ app/src/main/java/com/besttime/app/helpers/ContactImporter.java
@@ -1,6 +1,9 @@
 package com.besttime.app.helpers;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import com.besttime.models.Contact;
 
@@ -8,10 +11,33 @@ import java.util.ArrayList;
 
 public class ContactImporter {
 
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
     private ArrayList<Contact> contacts;
     private Context context;
+
 
     public ContactImporter(Context context) {
         this.context = context;
     }
+
+    /**
+     * Checks whether app has permission to read contacts.
+     * If not requests permission from user.
+     * @return true if permission is granted, false if not.
+     */
+    public boolean checkReadContactsPermission(){
+
+        if (context.checkSelfPermission(Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ((Activity)context).requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    PERMISSIONS_REQUEST_READ_CONTACTS);
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
