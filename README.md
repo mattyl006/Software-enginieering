8cb44126e2358f8a6c711589272f6a43bb9c1fff
Michał Czekański <michal.cz2202@gmai.com>
2020-01-04 16:31:00 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Kod źródłowy projektu BestTime.
---
+++ app/src/main/java/com/besttime/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.besttime.ui;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.besttime.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
