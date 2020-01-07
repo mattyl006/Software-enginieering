d4760c29a79e47b2c9a50af92722d2a0e77fa762
Michał Czekański <michal.cz2202@gmail.com>
2020-01-07 18:46:01 +0100

    Create empty adapter for contacts rec view

--- app/src/main/java/com/besttime/ui/adapters/ContactsRecyclerViewAdapter.java
+++ app/src/main/java/com/besttime/ui/adapters/ContactsRecyclerViewAdapter.java
@@ -1,4 +1,31 @@
 package com.besttime.ui.adapters;
 
-public class ContactsRecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.besttime.ui.adapters.viewHolders.ContactsViewHolder;
+import com.example.besttime.R;
+
+
+public class ContactsRecyclerViewAdapter extends RecyclerView.Adapter<ContactsViewHolder> {
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
