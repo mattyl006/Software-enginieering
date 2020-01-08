730eb573b075dacb2c0a7b79493420edc3848dea
Michał Czekański <michal.cz2202@gmail.com>
2020-01-08 13:37:47 +0100

    Close #1, close #2 (display contacts list)

--- app/src/main/java/com/besttime/ui/MainActivity.java
+++ app/src/main/java/com/besttime/ui/MainActivity.java
@@ -2,18 +2,59 @@ package com.besttime.ui;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.besttime.models.Contact;
+import com.besttime.ui.adapters.ContactsRecyclerViewAdapter;
 import com.example.besttime.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView contactsRecyclerView;
+    private ArrayList<Contact> contactsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeSampleDataAndAddItToContactsList();
+
+        contactsRecyclerView = findViewById(R.id.contactsRecyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        contactsRecyclerView.setLayoutManager(layoutManager);
+
+        ContactsRecyclerViewAdapter contactsAdapter = new ContactsRecyclerViewAdapter(contactsList);
+        contactsRecyclerView.setAdapter(contactsAdapter);
+
+    }
+
+
+
+    private void initializeSampleDataAndAddItToContactsList() {
+        Contact[] sampleData = new Contact[]{
+                new Contact(1, "John", "123456789"),
+                new Contact(2, "Mary", "987654321"),
+                new Contact(3, "Bob", "111222333"),
+                new Contact(4, "Marek", "444555666"),
+                new Contact(5, "Janek", "444555666"),
+                new Contact(6, "Anna", "444555666"),
+                new Contact(7, "Noname", "444555666")};
+
+
+        contactsList = new ArrayList<>();
+        for (Contact sampleContact: sampleData) {
+            contactsList.add(sampleContact);
+        }
 
     }
 }
