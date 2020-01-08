9c042626050d6f0df81b807bc4e58a99bb3e51e3
Michał Czekański <michal.cz2202@gmail.com>
2020-01-08 17:55:12 +0100

    Add getItemDetails in ContactsViewHolder
    Used by ItemDetailsLookup which will be used in selection tracker.

--- app/src/main/java/com/besttime/ui/adapters/viewHolders/ContactsViewHolder.java
+++ app/src/main/java/com/besttime/ui/adapters/viewHolders/ContactsViewHolder.java
@@ -6,6 +6,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.selection.ItemDetailsLookup;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.besttime.R;
@@ -30,5 +32,20 @@ public class ContactsViewHolder extends RecyclerView.ViewHolder {
     }
 
 
+    public ItemDetailsLookup.ItemDetails<Long> getItemDetails() {
+        ItemDetailsLookup.ItemDetails<Long> itemDetails = new ItemDetailsLookup.ItemDetails<Long>() {
+            @Override
+            public int getPosition() {
+                return getAdapterPosition();
+            }
 
+            @Nullable
+            @Override
+            public Long getSelectionKey() {
+                return (long)getAdapterPosition();
+            }
+        };
+
+        return itemDetails;
+    }
 }
