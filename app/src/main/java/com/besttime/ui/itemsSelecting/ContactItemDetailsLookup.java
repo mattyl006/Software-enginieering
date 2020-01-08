d1686be6d6a8b2f1573ad06c6919126c60e38277
Michał Czekański <michal.cz2202@gmail.com>
2020-01-08 17:53:57 +0100

    Override getItemDetails in ItemDetailsLookup

--- app/src/main/java/com/besttime/ui/itemsSelecting/ContactItemDetailsLookup.java
+++ app/src/main/java/com/besttime/ui/itemsSelecting/ContactItemDetailsLookup.java
@@ -1,11 +1,14 @@
 package com.besttime.ui.itemsSelecting;
 
 import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.selection.ItemDetailsLookup;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.besttime.ui.adapters.viewHolders.ContactsViewHolder;
 
 public class ContactItemDetailsLookup extends ItemDetailsLookup<Long> {
 
@@ -19,6 +22,14 @@ public class ContactItemDetailsLookup extends ItemDetailsLookup<Long> {
     @Nullable
     @Override
     public ItemDetails<Long> getItemDetails(@NonNull MotionEvent e) {
+        View view = recyclerView.findChildViewUnder(e.getX(), e.getY());
+        if(view != null){
+            RecyclerView.ViewHolder vh = recyclerView.getChildViewHolder(view);
+            if(vh instanceof ContactsViewHolder){
+                return ((ContactsViewHolder)vh).getItemDetails();
+            }
+        }
+
         return null;
     }
 }
