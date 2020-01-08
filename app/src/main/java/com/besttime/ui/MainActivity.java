d4aee6a53276bc5aff3ab19ed81ffa002c63bbf0
Michał Czekański <michal.cz2202@gmail.com>
2020-01-08 17:23:11 +0100

    Add custom action bar with just app name

--- app/src/main/java/com/besttime/ui/MainActivity.java
+++ app/src/main/java/com/besttime/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.besttime.ui;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar actionBar = findViewById(R.id.actionBar);
+        setSupportActionBar(actionBar);
+
 
         contactsList = new ArrayList<>();
 
