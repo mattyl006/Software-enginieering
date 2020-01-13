e32b342d93fa4badb56f4f93d84693a21480f1fb
Michał Czekański <michal.cz2202@gmail.com>
2020-01-13 19:06:12 +0100

    Create ContactEntry class with constructor

---
+++ app/src/main/java/com/besttime/app/ContactEntry.java
@@ -0,0 +1,14 @@
+package com.besttime.app;
+
+import com.besttime.models.Contact;
+
+public class ContactEntry {
+
+    private Contact contactInfo;
+    private int callCount;
+
+    public ContactEntry(Contact contactInfo, int callCount) {
+        this.contactInfo = contactInfo;
+        this.callCount = callCount;
+    }
+}
