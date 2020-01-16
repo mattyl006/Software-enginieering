dfefdd8bc4dd5797cd7e250b0497c23f10e3534e
Michał Czekański <michal.cz2202@gmail.com>
2020-01-16 18:37:02 +0100

    Add WhatsappContactIdRetriever helper class with method to retrieve id
    This class is needed, because some id is necessary when redirecting directly to whatsapp call.
    Also these ids can change over time, so they have to be retrieved
    dynamically, they can't be retrieved one time and stored somewhere.

---
+++ app/src/main/java/com/besttime/ui/helpers/WhatsappContactIdRetriever.java
@@ -0,0 +1,47 @@
+package com.besttime.ui.helpers;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+public class WhatsappContactIdRetriever {
+
+    String mimeStringWhatsappVoiceCall = "vnd.android.cursor.item/vnd.com.whatsapp.voip.call";
+    String mimeStringWhatsappVideoCall = "vnd.android.cursor.item/vnd.com.whatsapp.video.call";
+
+    private ContentResolver contentResolver;
+
+
+
+
+    public long getWhatsappVideoCallIdForContact(long contactId){
+
+        long whatsappId = -1;
+
+        String projection[] = {
+                ContactsContract.Data._ID,
+                ContactsContract.Data.MIMETYPE
+        };
+
+        String selection = ContactsContract.Data.CONTACT_ID + "=" + contactId;
+
+        Cursor cursor = contentResolver.query(
+                ContactsContract.Data.CONTENT_URI,
+                projection, selection, null, null);
+
+
+        int mimeTypeColInd = cursor.getColumnIndex(ContactsContract.Data.MIMETYPE);
+        String mimeType;
+
+
+        while (cursor.moveToNext()) {
+            mimeType = cursor.getString(mimeTypeColInd);
+            if(mimeType.equals(mimeStringWhatsappVideoCall)){
+                whatsappId = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data._ID));
+            }
+        }
+
+        return  whatsappId;
+    }
+}
