2cc2db500769e8b313f635d5a98a1697aac50582
Michał Czekański <michal.cz2202@gmail.com>
2020-01-16 19:01:18 +0100

    Add getters: ContactEntry and whatsappVideoCallId (viewModel class)

--- app/src/main/java/com/besttime/ui/viewModels/ContactEntryWithWhatsappId.java
+++ app/src/main/java/com/besttime/ui/viewModels/ContactEntryWithWhatsappId.java
@@ -1,5 +1,7 @@
 package com.besttime.ui.viewModels;
 
+import android.content.res.Resources;
+
 import com.besttime.app.ContactEntry;
 import com.besttime.ui.helpers.WhatsappContactIdRetriever;
 
@@ -16,5 +18,19 @@ public class ContactEntryWithWhatsappId {
     public ContactEntryWithWhatsappId(ContactEntry contactEntry, WhatsappContactIdRetriever whatsappContactIdRetriever) {
         this.contactEntry = contactEntry;
         this.whatsappContactIdRetriever = whatsappContactIdRetriever;
+    }
+
+    public ContactEntry getContactEntry() {
+        return contactEntry;
+    }
+
+    public long getWhatsappVideCallId(){
+        long whatsappVideoCallId = whatsappContactIdRetriever.getWhatsappVideoCallIdForContact(contactEntry.getContactId());
+
+        if(whatsappVideoCallId < 0){
+            // It is said that all contacts should be in whatsapp so no error handling.
+
+            // NOTHING
+        }
     }
 }
