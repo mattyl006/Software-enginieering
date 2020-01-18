3e30b3a4055fdbed3f0107df47a4c5a47788b456
Michał Czekański <michal.cz2202@gmail.com>
2020-01-18 19:45:48 +0100

    Add loading screen with app name

---
+++ app/src/main/java/com/besttime/ui/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.besttime.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.besttime.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
