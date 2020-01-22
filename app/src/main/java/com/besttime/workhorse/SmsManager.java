7ab391e80f96d9bf115d31d722f85a0becbb4ce1
Michał Czekański <michal.cz2202@gmail.com>
2020-01-22 20:42:45 +0100

    Add method checking send sms permission (SmsManager)

--- app/src/main/java/com/besttime/workhorse/SmsManager.java
+++ app/src/main/java/com/besttime/workhorse/SmsManager.java
@@ -1,5 +1,9 @@
 package com.besttime.workhorse;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+
 public class SmsManager {
 
     private android.telephony.SmsManager androidSmsManager;
@@ -10,5 +14,16 @@ public class SmsManager {
 
     public void sendSmsPrompt(Context context, String message){
         androidSmsManager.sendTextMessage(context.getContact().getContactNumber(), null, message, null, null);
+    }
+
+    public boolean checkSendSmsPermission(android.content.Context androidContext, int sendSmsRequestCode){
+        if (androidContext.checkSelfPermission(Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ((Activity)androidContext).requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+                    sendSmsRequestCode);
+            return false;
+        }
+        return true;
     }
 }
