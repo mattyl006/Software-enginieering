a9a6a948a25a6b7a1c12b9ef1ad5edb0ec2ab02d
Michał Czekański <michal.cz2202@gmail.com>
2020-01-25 20:44:12 +0100

    Add method checking if some forms have been updated and updates them (FormManager)

--- app/src/main/java/com/besttime/workhorse/FormManager.java
+++ app/src/main/java/com/besttime/workhorse/FormManager.java
@@ -1,22 +1,72 @@
 package com.besttime.workhorse;
 
+import com.besttime.workhorse.helpers.ParsedRowFromSheet;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class FormManager {
 
     private List<Form> sentForms;
+    private SheetsAndJava dataRetriever;
 
     public List<Form> getSentForms() {
         return sentForms;
     }
 
-    public FormManager(){
+    /**
+     *
+     * @throws IOException When there was error creating forms data retriever
+     * @throws GeneralSecurityException When there was error creating forms data retriever
+     */
+    public FormManager() throws IOException, GeneralSecurityException {
         this.sentForms = new ArrayList<>();
+        dataRetriever = new SheetsAndJava();
+    }
+
+
+    /**
+     * Checks whether some of the forms has been filled, fetches answers for these forms and updates them.
+     * @throws IOException When there was error checking responses
+     * @throws ParseException Error parsing row
+     */
+    public void checkResponses() throws IOException, ParseException {
+
+        List<List<Object>> dataFromSheets = dataRetriever.getAllFormsAnswers();
+
+        List<ParsedRowFromSheet> parsedRowsFromSheet = new ArrayList<>();
+
+        for (List<Object> notParsedRowFromSheet :
+                dataFromSheets) {
+            parsedRowsFromSheet.add(new ParsedRowFromSheet(notParsedRowFromSheet));
+        }
+
+
+        // Iterate over all parsed rows and check if there are some forms updated and update them
+        for (ParsedRowFromSheet parsedRow :
+                parsedRowsFromSheet) {
+
+            for (Form sentForm :
+                    sentForms) {
+
+                if(sentForm.getId() == parsedRow.getFormId()){
+                    if(!sentForm.hasResponded()){
+                        sentForm.updateAllDaysAnswers(parsedRow.getAnswers(), parsedRow.getDateWhenFormFilled());
+                    }
+                    else{
+                        if(sentForm.getDateWhenResponded().before(parsedRow.getDateWhenFormFilled())){
+                            sentForm.updateAllDaysAnswers(parsedRow.getAnswers(), parsedRow.getDateWhenFormFilled());
+                        }
+                    }
+                }
+
+            }
+        }
+
     }
 
 
@@ -41,10 +91,5 @@ public class FormManager {
         return null;
     }
 
-    private List<DayOfTheWeek> parseFetchedRowFromSheet(List<String> row){
-
-
-        return null;
-    }
 
 }
