831e893d5a930b0155027c7bcc8367f1d66a9bfe
Michał Czekański <michal.cz2202@gmail.com>
2020-01-25 15:00:50 +0100

    Add constructor and necessary fields in Form class

--- app/src/main/java/com/besttime/workhorse/Form.java
+++ app/src/main/java/com/besttime/workhorse/Form.java
@@ -1,4 +1,26 @@
 package com.besttime.workhorse;
 
+import java.util.Date;
+import java.util.List;
+
 public class Form {
+
+    private Context context;
+    private long id;
+    private boolean hasResponded;
+    private Date dateWhenResponded;
+    private List<DayOfTheWeek> allDaysAnswers;
+
+    public Form(long id, Context context) {
+        this.context = context;
+        this.id = id;
+        initializeEmptyAnswers();
+    }
+
+    private void initializeEmptyAnswers(){
+        for(int i = 0; i < 7; i ++){
+            DayOfTheWeek dayOfTheWeek = new DayOfTheWeek(i);
+            allDaysAnswers.add(dayOfTheWeek);
+        }
+    }
 }
