2dacb1124de85ffaec99c5cd77566814101ee96a
Michał Czekański <michal.cz2202@gmail.com>
2020-01-25 17:52:08 +0100

    Add code testing data that rows in sheets contain
    - If first cell contains convertable data
    - If last cell contains formId

--- app/src/test/java/com/besttime/workhorse/SheetsAndJavaTest.java
+++ app/src/test/java/com/besttime/workhorse/SheetsAndJavaTest.java
@@ -1,9 +1,13 @@
 package com.besttime.workhorse;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -11,7 +15,7 @@ import static org.junit.Assert.*;
 public class SheetsAndJavaTest {
 
     @Test
-    public void getAllFormsAnswers() throws IOException, GeneralSecurityException {
+    public void getAllFormsAnswers() throws IOException, GeneralSecurityException, ParseException {
         SheetsAndJava dataRetriever = new SheetsAndJava();
 
         List<List<Object>> allRows = dataRetriever.getAllFormsAnswers();
@@ -22,6 +26,16 @@ public class SheetsAndJavaTest {
                     row) {
                 assertEquals(cell.getClass(), String.class);
             }
+
+            try {
+                Date dateFilled = SheetsAndJava.dateFormat.parse((String) row.get(0));
+            } catch (ParseException e) {
+                Log.d("SheetsAndJavaTest", "No date or wrong date format in sheet");
+                throw e;
+            }
+
+            String formIdAsString = (String) row.get(row.size() - 1);
+            long formId = Long.parseLong(formIdAsString);
         }
     }
 }
