bb1bbbefd027a4fc520c4b8264f9f207f9d9ca1f
Michał Czekański <michal.cz2202@gmail.com>
2020-01-25 22:20:00 +0100

    Add new Json class

--- app/src/main/java/com/besttime/json/Json.java
+++ app/src/main/java/com/besttime/json/Json.java
@@ -1,96 +1,47 @@
 package com.besttime.json;
+import java.io.*;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+public class Json {
 
-import java.io.File;
-import java.io.IOException;
+    public void serialize(String name, Object object){
+        try{
+            FileOutputStream fs = new FileOutputStream(name+".ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(object);
+            os.close();
 
-public class Json<T> {
-
-    private String path; //path nie konczy sie "/"
-
-    public Json(String folderPath) {
-        this.path = folderPath;
-
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void serialize(String filename, T object) throws IOException {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
-            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-            objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-            objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-            objectMapper.writeValue(new File(this.getPath() + "/" + filename + ".json"), object);
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public T deserialize(String jsonPath) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
-        T object = null;
+    public Object deserialize(String name){
         try {
-            object = objectMapper.reader().forType(new TypeReference<T>() {
-            }).readValue(new File(this.getPath() + "/" + jsonPath + ".json"));
-        } catch (IOException e) {
+            FileInputStream strumienPlk = new FileInputStream(name+".ser");
+            ObjectInputStream os = new ObjectInputStream(strumienPlk);
+            Object obj = os.readObject();
+            return obj;
+        } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return object;
     }
+
+    /*
+    public static void main(String args[]){
+
+        Contact contact = new Contact(1,"Pawel", "535231060");
+        ContactEntry contactEntry = new ContactEntry(contact);
+
+        Json json = new Json();
+        json.serialize("asd", contactEntry);
+        ContactEntry con = (ContactEntry) json.deserialize("asd");
+        System.out.println(con.getAvailability().getCurrentDay());
+
+
+
+    }
+
+     */
 
 }
-//TODO  to jest main
-/*
-import java.io.IOException;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        Jc jc = new Jc("test1", "test2", 123);
-        Json json = new Json("..."); //tutaj sciezka do utworzonego jsona
-
-        json.serialize("jc2", jc);
-        System.out.println( json.deserialize("jc2")); // nazwa pliku json
-
-        System.out.println(jc.getX());
-
-
-    }
-}
-
-
-TODO to jest przykladowa klasa, ktora bedziemy serializowac i deserializowac
-
-public class Jc {
-
-    String x;
-    String y;
-    int z;
-
-    public Jc(String x1, String y1, int z1){
-        this.x = x1;
-        this.y = y1;
-        this.z = z1;
-    }
-
-        public String getX() {
-        return x;
-    }
-}
-
- */
