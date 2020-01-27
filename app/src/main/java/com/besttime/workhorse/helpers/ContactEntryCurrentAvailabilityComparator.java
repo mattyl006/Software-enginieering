29a32df7b532ed4ee0d69afc6709cd5824965793
Michał Czekański <michal.cz2202@gmail.com>
2020-01-27 19:07:09 +0100

    Add comparator class which compares ContactEntry objects based on their current Availability

---
+++ app/src/main/java/com/besttime/workhorse/helpers/ContactEntryCurrentAvailabilityComparator.java
@@ -0,0 +1,53 @@
+package com.besttime.workhorse.helpers;
+
+import com.besttime.app.ContactEntry;
+import com.besttime.workhorse.AvailType;
+
+import java.util.Comparator;
+
+public class ContactEntryCurrentAvailabilityComparator implements Comparator<ContactEntry> {
+    @Override
+    public int compare(ContactEntry o1, ContactEntry o2) {
+
+        AvailType o1AvailType = o1.getAvailability().getCurrentAvailability();
+        AvailType o2AvailType = o2.getAvailability().getCurrentAvailability();
+
+
+        if(o1AvailType.compareTo(o2AvailType) == 0){
+            return 0;
+        }
+        else{
+            switch (o1AvailType){
+
+                case unavailable:
+                    return -1;
+                case available:
+                    return 1;
+                case undefined:
+                    switch (o2AvailType){
+
+                        case unavailable:
+                            return 1;
+                        case available:
+                            return -1;
+                        case perhaps:
+                            return -1;
+                    }
+                    break;
+                case perhaps:
+                    switch (o2AvailType){
+
+                        case unavailable:
+                            return 1;
+                        case available:
+                            return -1;
+                        case undefined:
+                            return 1;
+                    }
+                    break;
+            }
+        }
+
+        return 0;
+    }
+}
