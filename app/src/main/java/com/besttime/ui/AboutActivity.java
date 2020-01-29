88239f22a091172238d7c99a9724fed93be3f946
Michał Czekański <michal.cz2202@gmail.com>
2020-01-29 18:32:21 +0100

    Add back button in about window (close #32)

--- app/src/main/java/com/besttime/ui/AboutActivity.java
+++ app/src/main/java/com/besttime/ui/AboutActivity.java
@@ -1,13 +1,19 @@
 package com.besttime.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.besttime.R;
 
 public class AboutActivity extends AppCompatActivity {
+
+
+    private Toolbar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,24 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        intitializeActionBar();
+
+
+    }
+
+    private void intitializeActionBar() {
+        actionBar = findViewById(R.id.actionBar_about);
+        setSupportActionBar(actionBar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        finish();
+        return true;
     }
 }
